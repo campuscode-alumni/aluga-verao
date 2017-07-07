f8f0535d6605c4e0d85fb21e302c66937b45c9ce
Eliza e Reinaldo <elizabethlopesramos@gmail.com>
2017-07-07 19:41:08 -0300

    ver os imoveis

--- spec/features/visitor_view_properties_spec.rb
+++ spec/features/visitor_view_properties_spec.rb
@@ -13,4 +13,21 @@ feature 'User view properties ' do
     expect(page).to have_link('sitio do meu vo')
   end
 
+  scenario 'and view details of properties' do
+    property = Property.create( city: 'SaoPaulo', state: 'SP', property_type: 'sitio', description: 'sitio do meu vo', price: 90.0, photo: 'sitio.jpg',
+                                capacity: 5, minimun_rent: 2, maximum_rent: 3, rules: 'varias regras mimimi', rent_purpose: 'ferias', owner: 'vo Carlos')
+
+    visit root_path
+
+    click_on 'sitio do meu vo'
+
+    expect(page).to have_css('h1', text: property.description)
+
+    expect(page).to have_css('li', text: property.state)
+    expect(page).to have_css('li', text: property.city)
+    expect(page).to have_css('li', text: property.price)
+    expect(page).to have_css('li', text: property.owner)
+    expect(page).to have_css('li', text:  property.rules)
+
+  end
 end
